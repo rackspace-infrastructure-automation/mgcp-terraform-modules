ff9854ab4a2431fc6a6276fd28e633354b24f8f9
pouya-racker <33907754+pouya-racker@users.noreply.github.com>
2020-02-14 08:27:49 -0800

    Revert "Add Partner Interconnect"

--- partner-interconnect/example/main.tf
+++
@@ -1,9 +0,0 @@
-module "test-partner-interconnect-module" {
-  source          = "../."
-
-  vlans             = ["test-vlan-a", "test-vlan-b"]
-  asn               = "16550"
-  advertise_mode    = "CUSTOM"
-
-  network           = "${var.network}"
-}
