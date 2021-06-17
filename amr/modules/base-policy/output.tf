1443621530d216f0fe6318283b7d179b9e86bb90
Leonardo Bertini <Leonardo.Bertini@rackspace.co.uk>
2021-06-17 13:12:07 +0100

    add outputs

--- amr/modules/base-policy/output.tf
+++ amr/modules/base-policy/output.tf
@@ -1,3 +1,23 @@
+output "rackspace_low_id" {
+  value = google_monitoring_notification_channel.rackspace_low.name
+}
+
+output "rackspace_normal_id" {
+  value = google_monitoring_notification_channel.rackspace_normal.name
+}
+
+output "rackspace_high_id" {
+  value = google_monitoring_notification_channel.rackspace_high.name
+}
+
+output "rackspace_high_id" {
+  value = google_monitoring_notification_channel.rackspace_high.name
+}
+
 output "rackspace_urgent_id" {
   value = google_monitoring_notification_channel.rackspace_urgent.name
+}
+
+output "rackspace_emergency_id" {
+  value = google_monitoring_notification_channel.rackspace_emergency.name
 }
