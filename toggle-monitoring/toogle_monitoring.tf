95bda80f5d03b5286e97bb6ebc39ce898ebc5656
leo-bertini <leonardo.bertini@rackpsace.co.uk>
2024-02-20 11:52:55 +0000

    add permissions

--- toggle-monitoring/toogle_monitoring.tf
+++ toggle-monitoring/toogle_monitoring.tf
@@ -1,31 +1,8 @@
-data "google_project" "project" {
-  project_id = var.project_id
-}
-
-module "projects_iam_bindings" {
-  source  = "terraform-google-modules/iam/google//modules/projects_iam"
-  version = "~> 7.7"
-
-  mode = "addititve"
-
-  projects = [var.project_id]
-
-  bindings = {
-    "roles/iam.serviceAccountTokenCreator" = [
-    "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com",
-    ]
-
-    "roles/run.serviceAgent" = [
-      "serviceAccount:service-${data.google_project.project.number}@serverless-robot-prod.iam.gserviceaccount.com",
-    ]
-  }
-}
-
 resource "google_cloudfunctions2_function" "toggle_monitoring_off" {
-  provider = google-beta
-  name     = "toggle-monitoring-off"
-  location = var.region
-  depends_on = [ module.projects_iam_bindings ]
+  provider   = google-beta
+  name       = "toggle-monitoring-off"
+  location   = var.region
+  depends_on = [google_project_iam_member.service_agent]
 
   build_config {
     runtime     = "python39"
@@ -57,10 +34,10 @@ resource "google_cloudfunctions2_function" "toggle_monitoring_off" {
 }
 
 resource "google_cloudfunctions2_function" "toggle_monitoring_on" {
-  provider = google-beta
-  name     = "toggle-monitoring-on"
-  location = var.region
-  depends_on = [ module.projects_iam_bindings ]
+  provider   = google-beta
+  name       = "toggle-monitoring-on"
+  location   = var.region
+  depends_on = [google_project_iam_member.service_agent]
 
   build_config {
     runtime     = "python39"
